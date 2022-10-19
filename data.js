b868c0c926e02e7aea6947ebf2b43dd2ca33bbb2
AHmyrianska <a.hmyrianska@gmail.com>
2022-10-19 16:08:32 +0300

    Working with Order section

---
+++ data.js
@@ -0,0 +1,23 @@
+export const items = [
+    {
+        name: 'Pizza',
+        image: 'images/pizza.png',
+        composition: 'pepperoni,mushrom,mozarella',
+        price: 14,
+        uuid: '22bf9ba5-3a22-4daa-8e0e-5618c0f0e50e'
+    },
+    {
+        name: 'Hamburger',
+        image: 'images/hamburger.png',
+        composition: 'beef, cheese, lettuce',
+        price: 12,
+        uuid: '6364ca4f-4d0b-4e1c-a5f9-3153ffd1f4ce'
+    },
+    {
+        name: 'Beer',
+        image: 'images/beer.png',
+        composition: 'grain, hops, yeast, water',
+        price: 12,
+        uuid: 'b53171c2-f1fb-41b1-a8ae-96711487cc0f'
+    }
+]
---
+++ script.js
@@ -0,0 +1,85 @@
+import { items } from '/data.js';
+
+let orderedArr = [];
+
+document.addEventListener('click', function(e) {
+    if(e.target.dataset.add){
+        handleAddBtn(e.target.dataset.add);
+    }
+})
+
+function handleAddBtn(itemId) {
+    let targetItem = items.filter(function(item) {
+        return item.uuid === itemId;
+    })[0]
+
+    orderedArr.push(targetItem);
+
+    renderOrder(orderedArr) 
+
+    // for (let item of orderedArr) {
+    //     console.log(orderedArr);
+    // let orderedItem = document.createElement('div');
+    // orderedItem.classList.add('ordered-item');
+    // orderedItem.innerHTML = `
+    //     <div class="order-item">
+    //         <h3>${item.name}</h3>
+    //         <p class="remove">remove</p>
+    //         <p class="price">${item.price}</p>
+    //     </div>
+    // `
+    // document.getElementById('order-wrapper').append(orderedItem);
+    // }
+
+}
+
+function renderOrder(list) {
+    let orderedItem = document.createElement('div');
+    orderedItem.classList.add('ordered-item');
+
+    for (let item of list) {
+        orderedItem.innerHTML = `
+            <div class="order-item">
+                <h3>${item.name}</h3>
+                <p class="remove">remove</p>
+                <p class="price">$${item.price}</p>
+            </div>
+        `
+        document.getElementById('order-wrapper').append(orderedItem); 
+    }
+}
+
+function getFeedHtml() {
+    let feedHtml = ``;
+
+    for (let item of items) {
+        
+        feedHtml += `
+        <div class="menu-item">
+            <div class="img-wrapper">
+                <img src="${item.image}" alt="${item.name}">
+            </div>
+            <div class="description">
+                <h3>${item.name}</h3>
+                <p class="ingredients">${item.composition}</p>
+                <p class="price">$${item.price}</p>
+            </div>
+            <div class="btn-wrapper">
+                <button type="button" class="add-btn" data-add="${item.uuid}">+</button>
+            </div>
+        </div>
+        <hr>
+        `       
+ 
+    }
+    
+    return feedHtml;
+}
+
+function renderMenu () {
+    document.getElementById('menu-container').innerHTML = getFeedHtml();
+}
+
+renderMenu();
+
+
